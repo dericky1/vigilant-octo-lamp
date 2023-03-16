cdd26f13182031f85a8d1e69ad4960999d0ff3a6
Derick Young <young.derickj@gmail.com>
2023-03-16 15:51:51 -0600

    Add comments, change API key and add .env

--- src/components/GifSearch.tsx
+++ src/components/GifSearch.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 
+//structure of each gif
 interface Gif {
   id: string;
   url: string;
@@ -11,12 +12,15 @@ const MyComponent: React.FC = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const containerRef = useRef<HTMLDivElement>(null);
-  const isSearchQuery = searchQuery.trim() !== "";
+  const isSearchQuery = searchQuery.trim() !== ""; // a check to see if the search query is empty
+  const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
 
+  // Function call giphy api to get gifs based on searchQuery, limit of 10, and the current page
+  // Sets our state variable gifs to contain all the data
   const fetchData = async () => {
     try {
       const response = await fetch(
-        `https://api.giphy.com/v1/gifs/search?api_key=HlD75vhZn7G8rI78fRoRLQaettAcYjxu&q=${searchQuery}&limit=10&offset=${
+        `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchQuery}&limit=10&offset=${
           (currentPage - 1) * 10
         }`
       );
@@ -34,12 +38,26 @@ const MyComponent: React.FC = () => {
   };
 
   const handleSearch = async () => {
-    setGifs([]);
+    setGifs([]); // This clears the old search info so that we only display gifs that we searched for
+    setCurrentPage(0) // Set page back to page 0 so we don't miss the first page of data
     await fetchData();
+  };
+
+  const handleScroll = async (event: Event) => {
+    const container = event.target as HTMLDivElement;
+    // Check if the user has scrolled to the bottom of the div
+    // scrollHeight the height of the entire content of the div
+    // scrollTop distance between top of the div and the visible content (gets larger as user scrolls down)
+    // clientHeight is the height of the visible portion of the div
+    if (container.scrollHeight - container.scrollTop === container.clientHeight) {
+      // If so, load the next page of data
+      await fetchData();
+    }
   };
 
   useEffect(() => {
     // Add event listener to detect when the user has scrolled to the bottom of the div
+    // When the user scrolls to the bottom of the div handleScroll is called
     const container = containerRef.current;
     container?.addEventListener("scroll", handleScroll);
 
@@ -48,15 +66,6 @@ const MyComponent: React.FC = () => {
       container?.removeEventListener("scroll", handleScroll);
     };
   }, [currentPage]);
-
-  async function handleScroll(event: Event) {
-    const container = event.target as HTMLDivElement;
-    // Check if the user has scrolled to the bottom of the div
-    if (container.scrollHeight - container.scrollTop === container.clientHeight) {
-      // If so, load the next page of data
-      await fetchData();
-    }
-  }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-tealgreen">
@@ -77,12 +86,15 @@ const MyComponent: React.FC = () => {
             Search
           </button>
         </div>
+        {/* If the gifs array == [] i.e. nothing had been search, dont display anything*/}
         {gifs.length !== 0 ? (
           <div className="rounded bg-lightorange">
             <div
               ref={containerRef}
               className="grid grid-cols-2 md:grid-cols-5 gap-4 h-96 overflow-y-scroll scrollbar-none m-6"
             >
+              {" "}
+              {/* Mapping through gifs and displaying each gif in a grid*/}
               {gifs.map((gif) => (
                 <a key={gif.id} href={gif.url}>
                   <img key={gif.id} src={gif.url} alt={gif.title} className="w-48 h-48 rounded" />
